e5631acb5334c12d718bd695f977445b5da0ca6c
mmalkiew <michalmalkiewicz90@gmail.com>
2019-01-23 20:12:04 +0100

    register all services to eureka naming server

--- currency-conversion-service/src/main/java/pl/mmalkiew/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/pl/mmalkiew/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package pl.mmalkiew.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("pl.mmalkiew.microservices.currencyconversionservice.proxy")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
