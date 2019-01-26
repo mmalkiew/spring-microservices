b8e25bc120f8c3d802166b72d05e4b4bd612776a
mmalkiew <michalmalkiewicz90@gmail.com>
2019-01-26 19:13:13 +0100

    Setting up ZUUL API Gateway between microservices invocations

--- currency-conversion-service/src/main/java/pl/mmalkiew/microservices/currencyconversionservice/proxy/CurrencyExchangeFeignClient.java
+++ currency-conversion-service/src/main/java/pl/mmalkiew/microservices/currencyconversionservice/proxy/CurrencyExchangeFeignClient.java
@@ -8,11 +8,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import pl.mmalkiew.microservices.currencyconversionservice.model.CurrencyConversionResponse;
 
 //@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
-@FeignClient(name = "currency-exchange-service")
+/*
+it could be used in direct invocation between microservices
+ */
+//@FeignClient(name = "currency-exchange-service")
+
+/**
+ * Setup ZUUL API gateway between microservices application
+ */
+@FeignClient(name = "api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeFeignClient {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+//    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     CurrencyConversionResponse retrieveExchangeValue(@PathVariable String from,
                                                      @PathVariable String to);
 }
