811f6ba805d752bd487b87c3cc14af65e3705976
mmalkiew <michalmalkiewicz90@gmail.com>
2019-01-26 18:43:15 +0100

    create api-gateway-server project - ZUUL

---
+++ api-gateway-server/src/main/java/pl/mmalkiew/microservices/apigatewayserver/ApiGatewayServerApplication.java
@@ -0,0 +1,18 @@
+package pl.mmalkiew.microservices.apigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayServerApplication.class, args);
+    }
+
+}
+
