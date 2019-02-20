9c82e70c89d24fea5097ae7490d4cd9e5f7b8fae
mmalkiew <michalmalkiewicz90@gmail.com>
2019-02-20 23:09:57 +0100

    enable hystrix - fallback default configuration example

--- limit-service/src/main/java/pl/mmalkiew/microservices/limitservice/config/LimitsConfigurationController.java
+++ limit-service/src/main/java/pl/mmalkiew/microservices/limitservice/config/LimitsConfigurationController.java
@@ -1,11 +1,16 @@
 package pl.mmalkiew.microservices.limitservice.config;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LimitsConfigurationController.class);
 
     @Autowired
     private Configuration configuration;
@@ -14,6 +19,18 @@ public class LimitsConfigurationController {
     public LimitConfiguration retrieveLimitsFromConfigurations() {
         LimitConfiguration limitConfiguration = new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
         return limitConfiguration;
+    }
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod="fallbackDefaultConfiguration")
+    public LimitConfiguration getConfiguration() {
+        LOGGER.error("getting configuration error. Configuration not available ...");
+        throw new RuntimeException("Not available");
+    }
+
+    public LimitConfiguration fallbackDefaultConfiguration() {
+        LOGGER.info("get default configuration ...");
+        return new LimitConfiguration(999, 9);
     }
 
 
